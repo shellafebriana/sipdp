2599d4e0179265a75a9e03f31057d1cf7c70032f
Shella Febriana <shellafebriana14@gmail.com>
2022-12-26 23:54:17 +0700

    perbaiki sub karena trace

--- sub/footer.php
+++ sub/footer.php
@@ -57,11 +57,6 @@
 
 <!-- Page level custom scripts -->
 <script src="../assets/js/demo/datatables-demo.js"></script>
-<<<<<<< HEAD
-=======
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
->>>>>>> 2811d18b15e7f68000fec5e899f796beafc76090
-
 <script type="text/javascript">
     $('#kategori').change(function() {
         var kategori = $(this).val();
